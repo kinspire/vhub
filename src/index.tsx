c3adc2c09681eabbde4f77ecb94d9030407c20df
Sarang Joshi <sarangj@msn.com>
2018-12-18 14:36:25 -0800

    Add basename for gh-pages

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/vhub">
     <App />
   </BrowserRouter>,
   document.getElementById("root") as HTMLElement
