070cb52230d42f6f961cd6639452ce36d9f5c3b5
Sarang Joshi <sarangj@msn.com>
2018-12-22 15:22:12 -0800

    Organize content page

--- src/pages/Content.tsx
+++ src/pages/Content.tsx
@@ -1,21 +1,23 @@
-import * as firebase from "firebase";
+import { firestore, Unsubscribe } from "firebase";
+import * as _ from "lodash";
+import * as log from "loglevel";
 import * as React from "react";
 import { Link } from "react-router-dom";
 
 import * as fs from "../firebaseService";
 
 export interface IState {
-  content: firebase.firestore.QueryDocumentSnapshot[];
+  content: Array<Array<Record<string, firestore.DocumentSnapshot>>>;
 }
 
 export default class Content extends React.Component<{}, IState> {
-  private unregisterObserver: firebase.Unsubscribe;
+  private unregisterObserver: Unsubscribe;
 
   constructor(props: any) {
     super(props);
 
     this.state = {
-      content: []
+      content: [],
     };
   }
 
@@ -24,8 +26,23 @@ export default class Content extends React.Component<{}, IState> {
       .db()
       .collection("content")
       .onSnapshot(snapshot => {
+        const contentBuckets: Array<
+          Array<Record<string, firestore.DocumentSnapshot>>
+        > = [];
+
+        snapshot.docs.forEach(doc => {
+          if (!contentBuckets[doc.get("classLevel")]) {
+            contentBuckets[doc.get("classLevel")] = [];
+          }
+          const contentBucket: any[] = contentBuckets[doc.get("classLevel")];
+          if (!contentBucket[doc.get("num")]) {
+            contentBucket[doc.get("num")] = {};
+          }
+          const content: any = contentBucket[doc.get("num")];
+          content[doc.get("type")] = doc;
+        });
         this.setState({
-          content: snapshot.docs
+          content: contentBuckets,
         });
       });
   }
@@ -38,15 +55,34 @@ export default class Content extends React.Component<{}, IState> {
     return (
       <div>
         Content:
-        <ul>
-          {this.state.content.map(c => (
-            <li>
-              <Link to={`/editcontent/${c.id}`}>
-                {c.get("title")} &mdash; {c.get("type")}
-              </Link>
-            </li>
-          ))}
-        </ul>
+        {this.state.content.map((bucket, classLevel) =>
+          bucket ? (
+            <div key={classLevel}>
+              <h1>Class Level {classLevel}</h1>
+              {bucket.map((coll, num) => {
+                log.debug(coll);
+                return coll ? (
+                  <div key={num}>
+                    <h2>Number {num}</h2>
+                    <ul>
+                      {_.map(coll, (c, i) => (
+                        <li key={i}>
+                          <Link to={`/editcontent/${c.id}`}>
+                            {c.get("title")} &mdash; {c.get("type")}
+                          </Link>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                ) : (
+                  ""
+                );
+              })}
+            </div>
+          ) : (
+            ""
+          )
+        )}
       </div>
     );
   }
